38c28d3f8edd62ca60820b05029e203a387a4247
ridzkyan0504-arch <ridzkyan0504@gmail.com>
2026-09-12 18:32:26 +0700

    learn: python variabel dan tipe data

---
+++ python/01-dasar/01-variabel.py
@@ -0,0 +1,9 @@
+nama = 'ridzkyan'
+umur = 22
+tinggi = 167.0
+status_kuliah = True
+
+print(f'nama:  {nama} {type(nama)}')
+print(f'umur:  {umur} {type(umur)}')
+print(f'tinggi:  {tinggi} {type(tinggi)}')
+print(f'status kuliah:  {status_kuliah} {type(status_kuliah)}')
